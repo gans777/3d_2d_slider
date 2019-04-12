64f9e1b8e1dea1e01ecb5a2a1e6bf1bf6a28e1ee
gans777 <animekogan@gmail.com>
2019-04-12 18:16:58 +0300

    z

--- slider_2d.js
+++ slider_2d.js
@@ -12,14 +12,19 @@ $(document).ready(function($){
 
     console.log('изобр 6 =' + vseImg[6]);
     console.log(vseImg[0]);
-    $('.slider_ekran').append($("<img src='"+vseImg[0]+"' >").css({
-        "position":"absolute",
-        "top": 0,
-        "left": "500px",
-        "width":"300px",
-        "zIndex": 7
 
+
+    /* второй от центра лево */
+
+    $('.slider_ekran').append($("<img src='"+vseImg[0]+"' >").css({ //последний слева
+        "position":"absolute",
+        "top": "70px",
+        "left": "450px",
+        "width":"100px",
+        "zIndex": 5
     }));
+
+    /* первый от центра  лево*/
 
     $('.slider_ekran').append($("<img src='"+vseImg[1]+"' >").css({
         "position":"absolute",
@@ -28,30 +33,59 @@ $(document).ready(function($){
         "width":"200px",
         "zIndex": 6
     }));
-
-    $('.slider_ekran').append($("<img src='"+vseImg[2]+"' >").css({
+    /* центр */
+    $('.slider_ekran').append($("<img src='"+vseImg[2]+"' >").css({ // центральный
         "position":"absolute",
-        "top": "70px",
-        "left": "450px",
-        "width":"100px",
-        "zIndex": 5
+        "top": 0,
+        "left": "500px",
+        "width":"300px",
+        "zIndex": 7
+
     }));
-    /* право */
-    $('.slider_ekran').append($("<img src='"+vseImg[1]+"' >").css({
+
+    /* первый отцентра право */
+    $('.slider_ekran').append($("<img src='"+vseImg[3]+"' >").css({
         "position":"absolute",
         "top": "40px",
-        "left": "670px",
+        "left": "620px",
         "width":"200px",
         "zIndex": 6
     }));
 
-    $('.slider_ekran').append($("<img src='"+vseImg[2]+"' >").css({
+    /* второй от центра право */
+    $('.slider_ekran').append($("<img src='"+vseImg[4]+"' >").css({
         "position":"absolute",
         "top": "70px",
-        "left": "650px",
+        "left": "740px",
         "width":"100px",
         "zIndex": 5
     }));
 
+    /*переход от правого первого к центру */
+    $('.slider_ekran>img[src="'+vseImg[3]+'"]').animate({
+        top: 0,
+        left: "500px",
+        width:"300px",
+        opacity: 0.6,
 
+    },1000, function() {
+        $('.slider_ekran>img[src="'+vseImg[3]+'"]').animate({
+            zIndex: 6,
+            opacity: 1
+        });
+    });
+
+    //css("border","5px solid red");
+     /* Переход от центра к левому первому */
+     $('.slider_ekran>img[src="'+vseImg[2]+'"]').animate({
+         width: "200px",
+         left: "470px",
+         top: "40px",
+         opacity: 0.5,
+         zIndex: 6
+     },1000, function(){
+         $('.slider_ekran>img[src="'+vseImg[2]+'"]').animate({
+             opacity: 1
+         });
+     }); //css("border","2px solid red");
 });
